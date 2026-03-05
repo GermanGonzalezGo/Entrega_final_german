9142417ce8f49cfafc6606849a7d510178bc467f
GermanGonzalezGo <gonzalezgomezgerman901@gmail.com>
2026-03-05 18:35:59 +0000

    Cambios nuevos

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Entrega_final_german
-Esta es mi entrega final de Taller Multimedia y Transmedia.
+Esta es mi entrega final de Taller Multimedia y Transmedia
---
+++ script.js
@@ -0,0 +1,17 @@
+const varTitulo = document.getElementById("titulo");
+let textoInicial = true; // variable de tipo booleano: true or false
+
+varTitulo.addEventListener("click", ()=> {
+    //el punto es algo que le pertenece a esa variable
+   
+    //console.log("hiciste click");
+    //alert("hiciste click")
+    if(textoInicial === true){
+        varTitulo.innerText = "El cuerpo duele";
+       // textoInicial = false;
+    } else {
+        varTitulo.innerText = "El cuerpo recuerda";
+      //  textoInicial = true
+    }
+    textoInicial = !textoInicial;
+});
