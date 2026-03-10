d54ffdd8b0ffb8d0d8817e3ce960602a5ff94d14
GermanGonzalezGo <gonzalezgomezgerman901@gmail.com>
2026-03-10 11:39:10 +0000

    Nueva interfaz y audio

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// Código para index.html---------------------------------------------------------------------------------
 const varTitulo = document.getElementById("titulo");
+const sonido = document.getElementById("sonido");
+
 let textoInicial = true; // variable de tipo booleano: true or false
+let sonidoActivado = false;
 
 varTitulo.addEventListener("click", ()=> {
     //el punto es algo que le pertenece a esa variable
@@ -14,4 +18,12 @@ varTitulo.addEventListener("click", ()=> {
       //  textoInicial = true
     }
     textoInicial = !textoInicial;
+    
+    if (sonidoActivado === false){ //es un condicional dentro de la función, así que se activa siempre y cuando el sonido no se haya activado
+        sonido.play(); //reproduce el audio cuando se hace clic en el título
+        sonidoActivado = true;
+    }
 });
+
+// Código para memoria.html---------------------------------------------------------------------------------
+const memorias =
